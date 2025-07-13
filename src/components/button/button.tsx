4fbaa1f7a6a0247ca85dcdeb4f6a86251f753f3e
Vl Lena <tg @vlpro>
2025-07-13 17:25:40 +0300

    feat: add static layout

---
+++ src/components/button/button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+interface ButtonProps {
+  btnName: string;
+}
+
+class Button extends React.Component<ButtonProps> {
+  render() {
+    return <button>{this.props.btnName}</button>;
+  }
+}
+
+export default Button;
---
+++ src/components/card/card.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+class Card extends React.Component {
+  render() {
+    return (
+      <div className="card">
+        <img src="" alt="photo" />
+        <div>
+          <p>Opisanie</p>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Card;
