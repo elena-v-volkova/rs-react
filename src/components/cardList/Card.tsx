3f1fdcb48739b5b2f50c891ea387ead2d69d56bf
Vl Lena <tg @vlpro>
2025-08-04 00:45:10 +0300

    feat: add downloading

--- src/components/cardList/Card.tsx
+++ src/components/cardList/Card.tsx
@@ -1,3 +1,4 @@
+import { useSelectedItemsStore } from '../../store/itemStore';
 import type { CardProps } from './types';
 
 export default function Card(props: CardProps) {
@@ -9,8 +10,23 @@ export default function Card(props: CardProps) {
     status: { data: character.status, color: 'blue' },
   };
 
+  const selectedItems = useSelectedItemsStore((s) => s.selectedItems);
+  const selectItem = useSelectedItemsStore((s) => s.selectItem);
+  const unselectItem = useSelectedItemsStore((s) => s.unselectItem);
+
+  const isSelected = !!selectedItems[props.character.id];
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.checked) {
+      selectItem(props.character);
+    } else {
+      unselectItem(props.character.id);
+    }
+  };
+
   return (
     <div className="card" onClick={onClick}>
+      <input type="checkbox" onChange={handleChange} checked={isSelected} />
       <img src={character.image} alt={character.name} width={100} />
       <h3>{character.name}</h3>
       <div className="card-description">
